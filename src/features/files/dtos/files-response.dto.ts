f3029e18b52aef04f4a8af66b8786f7f81420e4a
Felipe Duaibs <felipe.duaibs@hotmail.com>
2024-06-05 14:54:49 -0300

    feat: pagination metadata on files

--- src/features/files/dtos/files-response.dto.ts
+++ src/features/files/dtos/files-response.dto.ts
@@ -1,6 +1,7 @@
 import {
   GetAllFileDataResponseInterface,
   GetFileDataResponseInterface,
+  PaginationMetadataInterface,
 } from '../../../repository/firebase-storage-repository/interfaces/firebase-storage-repository-response.interface';
 
 export class FindOneFileResponseDto implements GetFileDataResponseInterface {
@@ -13,6 +14,11 @@ export class FindOneFileResponseDto implements GetFileDataResponseInterface {
   updatedAt: string;
 }
 
+export class PaginationMetadataDto implements PaginationMetadataInterface {
+  count: number;
+}
+
 export class FindAllFilesResponseDto implements GetAllFileDataResponseInterface {
   fileDataList: FindOneFileResponseDto[];
+  metadata: PaginationMetadataDto;
 }
