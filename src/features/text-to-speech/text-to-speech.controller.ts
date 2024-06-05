449145334d61a8d1f27239b7ad0ca4bf03ab0b46
Felipe Duaibs <51001493+fduaibs@users.noreply.github.com>
2024-06-05 20:24:04 -0300

    Update text-to-speech.controller.ts

--- src/features/text-to-speech/text-to-speech.controller.ts
+++ src/features/text-to-speech/text-to-speech.controller.ts
@@ -1,20 +1,11 @@
-import { Body, Controller, HttpCode, HttpStatus, Post, StreamableFile } from '@nestjs/common';
-import { ApiBasicAuth, ApiTags } from '@nestjs/swagger';
-import { Readable } from 'stream';
-import { TextToSpeechSynthesizeDto } from './dtos/text-to-speech.dto';
-import { TextToSpeechService } from './text-to-speech.service';
-
-@Controller('text-to-speech')
 @ApiTags('Text To Speech Controller')
 @ApiBasicAuth()
 export class TextToSpeechController {
   constructor(private readonly textToSpeechService: TextToSpeechService) {}
 
-  @Post('synthesize')
+  @Post('sinthesize')
   @HttpCode(HttpStatus.OK)
-  async create(@Body() synthesizeDto: TextToSpeechSynthesizeDto): Promise<StreamableFile> {
-    const fileBuffer = await this.textToSpeechService.synthesize(synthesizeDto.text, synthesizeDto.contentType);
-
-    return new StreamableFile(fileBuffer as Readable);
+  async create(@Body() sinthesizeDto: TextToSpeechSynthesizeDto): Promise<TextToSpeechSinthesizeResponseDto> {
+    return await this.textToSpeechService.synthesize(sinthesizeDto.text, sinthesizeDto.contentType);
   }
 }
